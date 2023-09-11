78fe9c2ece014d90e1d0dd2de61c72e8f1792bc5
leducthanh98 <70497414+leducthanh98@users.noreply.github.com>
2023-09-12 03:44:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,24 +25,6 @@ install_3proxy() {
     cd $WORKDIR
 }
 
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 1000
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-flush
-allow *
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
 }
 
 gen_proxy_file_for_user() {
