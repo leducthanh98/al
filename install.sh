9782f2b31952df6403cb436e4ae0780e984a3057
leducthanh98 <70497414+leducthanh98@users.noreply.github.com>
2023-09-12 03:23:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,13 +34,13 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth strong
+allow *
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"proxy -6 -n -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
